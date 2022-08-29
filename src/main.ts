7aa1e1f2132cb2e657918c0cea13958ff3a2474f
Maxime Riaud <maxime.riaud@gmail.com>
2022-08-29 17:37:30 +0200

    creation d'une fonction ls recursive

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import { App, Editor, MarkdownEditView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, View } from 'obsidian';
+import { App, Editor, MarkdownEditView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, View, FileSystemAdapter } from 'obsidian';
 import { WebsocketProvider } from "y-websocket"
 import * as Y from 'yjs'
 // @ts-ignore
 import { yCollab } from 'y-codemirror.next'
-
-import { EditorView, basicSetup } from "codemirror";
+// import { EditorView, basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
-
-import * as random from 'lib0/random'
 import { YText } from 'yjs/dist/src/internals';
 
 interface MyPluginSettings {
@@ -26,7 +23,8 @@ class DistantFile {
 	plugin: EditorState.Extension;
 
     constructor(doc_name: string) {
-		this.ydoc =  new Y.Doc({guid: doc_name});
+		this.ydoc =  new Y.Doc();
+		this.ydoc
 		this.yText = this.ydoc.getText();
 		this.provider = new WebsocketProvider('ws://localhost:8080', doc_name, this.ydoc);
 		this.plugin = yCollab(this.yText, this.provider.awareness);
@@ -40,29 +38,61 @@ class DistantFile {
 	}
 }
 
+async function recursiveLs(path: string, fs: FileSystemAdapter, level: number)
+{
+	const list = await fs.list(path);
+	let tab = "";
+	for (let i = 0; i < level; i++) {
+		tab += "\t";
+	}
+	list.files.forEach(file => {
+		console.log(tab + file);
+	})
+	level++;
+	list.folders.forEach(async folder => {
+		if (tab + folder)
+		recursiveLs(folder, fs, level);
+	})
+}
+
 export default class MyPlugin extends Plugin {
-	openedFiles: Map<CodeMirror.Doc, DistantFile> = new Map();
-	plugs: Map<TFile, EditorState.Extension> = new Map();
+	ydocs: Map<TFile, YText> = new Map();
+	openedFiles: Map<TFile, DistantFile> = new Map();
+	plugs: Array<EditorState.Extension> = [];
+	current_file: TFile;
 	settings: MyPluginSettings;
+	fs: FileSystemAdapter = new FileSystemAdapter(".");
+
+	updateSyncEngine(file: TFile | null): void {
+		if (!file)
+			return ;
+		if (!this.openedFiles.has(file))
+		{
+			this.openedFiles.set(file, new DistantFile(file.path));
+			console.log("Register a new file: " + file.name);
+		}
+		if (this.current_file != file)
+		{
+			this.plugs[0] = this.openedFiles.get(file)?.getPlugin();
+			this.app.workspace.updateOptions();
+			this.current_file = file;
+			console.log("update view");
+		}
+	}
 
 	async onload() {
-		this.registerEvent(this.app.workspace.on("editor-change", (editor, markdownView) => {
-			this.app.workspace.getLeavesOfType("markdown").forEach(leave => {
-				const viewState = leave.getViewState();
-				console.log(leave);
-				// if (viewState.state == "markdown" && !this.openedFiles.has(viewState.state))
-				// {
-				// 	const doc = cm.getDoc();
-				// 	this.openedFiles.set(doc, new DistantFile(cm.get));
-				// 	this.plugs.set(markdownView.file, this.openedFiles.get(markdownView.file)?.getPlugin());
-					this.registerEditorExtension([...this.plugs.values()]);
-				// 	this.app.workspace.updateOptions();
-				// 	console.log("Register a new file: " + markdownView.file.name);
-				// }
+		const basePath = (this.app.vault.adapter as any).basePath;
+		console.log("loaded");
+		console.log(basePath);
+		recursiveLs(basePath, this.fs, 0);
 
-			})
-			console.log('diff');
-		}));
+		// this.registerEditorExtension(this.plugs);
+		// this.registerEvent(this.app.workspace.on("editor-change",
+		// 	(_, markdownView) => this.updateSyncEngine(markdownView.file)
+		// ));
+		// this.registerEvent(this.app.workspace.on("file-open",
+		// 	file => this.updateSyncEngine(file)
+		// ));
 
 		await this.loadSettings();
 	}
