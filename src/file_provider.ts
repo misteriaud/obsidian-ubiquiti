7fcd4fa8b8121776743c8407fb6bd8c6cda03051
Maxime Riaud <maxime.riaud@gmail.com>
2022-09-01 14:17:29 +0200

    using ytext

--- src/file_provider.ts
+++ src/file_provider.ts
@@ -1,28 +1,74 @@
 import * as Y from 'yjs';
-import { FileSystemAdapter } from 'obsidian';
+import { App, TFile } from 'obsidian';
+import { YText } from 'yjs/dist/src/internals';
 import { Observable } from 'lib0/observable.js';
+import { YDelta, getDeltaOperations } from "./getDeltas"
 
-async function recursiveLs(path: string, fs: FileSystemAdapter, level: number)
-{
-	if (path.contains("obsidian-ubiquiti"))
-		return ;
-	const list = await fs.list(path);
-	list.files.forEach(async file => {
-		console.log(await fs.stat(file));
-	})
-	level++;
-	list.folders.forEach(async folder => {
-		if (folder)
-			recursiveLs(folder, fs, level);
-	})
-}
-
-export class FilePersistence extends Observable {
-	name: String;
+export class ObsidianProvider extends Observable<string> {
+	app: App;
 	doc: Y.Doc;
-	vault_path: String;
-	fs: FileSystemAdapter = new FileSystemAdapter();
-	constructor(name: String, doc: Y.Doc, vault_path: String) {
-		super()
+	// vault_path: String;
+	constructor(app: App, doc: Y.Doc, vault_path: String) {
+		super();
+		this.app = app;
+		this.doc = doc;
+		this.initDoc();
+
+		// When documents are modified locally
+		this.app.vault.on("modify", async (file: TFile) => this.populateModifToYdoc(file), this);
+
+		//When documents are modifier remotely
+		doc.on('update', (update, origin) => {
+			if (origin === this)
+				return ;
+			this.emit('update', [update])
+		})
+	}
+
+	destroy(): void {
+		this.doc.off("update", () => {});
+	}
+
+	async initDoc() {
+		let current_text: YText;
+		this.app.vault.getMarkdownFiles().forEach(async file => {
+			current_text = this.doc.getText(file.path);
+			current_text.insert(0, await this.app.vault.read(file));
+		})
+	}
+
+	async populateModifToYdoc(file: TFile) {
+		if (!this || !this.doc)
+			return ;
+		console.log('update');
+		let current_text: YText = this.doc.getText(file.path);
+		let delta: YDelta[] = [];
+		delta = getDeltaOperations(current_text.toString(), await this.app.vault.read(file));
+		if (delta.length > 0)
+			current_text.applyDelta(delta);
+	}
+
+	async populateModifToFile(path: string, ytext: Y.YTextEvent) {
+		console.log(path);
+		console.log(ytext.changes.delta);
+		// if (!this || !this.ydoc)
+		// 	return ;
+		// let current_text: YText = this.ydoc.getText(file.path);
+		// let delta: YDelta[] = [];
+		// delta = getDeltaOperations(current_text.toString(), await this.app.vault.read(file));
+		// if (delta.length > 0)
+		// 	current_text.applyDelta(delta);
+	}
+
+	get (key: String | number | ArrayBuffer | Date) {
+		console.log("provider get: " + key);
+	}
+
+	set (key: String | number | ArrayBuffer | Date, value: String | number | ArrayBuffer | Date) {
+		console.log("provider set: " + key + "\nvalue: " + value);
+	}
+
+	del(key: String | number | ArrayBuffer | Date) {
+		console.log("provider del: " + key);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import * as Y from 'yjs'
 import { yCollab } from 'y-codemirror.next'
 // import { EditorView, basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
-import { YMap, YText } from 'yjs/dist/src/internals';
-import { YDelta, getDeltaOperations } from "./getDeltas"
+import { ObsidianProvider } from "./file_provider"
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -16,91 +15,24 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-// async function recursiveLs(path: string, fs: FileSystemAdapter, level: number)
-// {
-// 	if (path.contains("obsidian-ubiquiti"))
-// 		return ;
-// 	const list = await fs.list(path);
-// 	list.files.forEach(async file => {
-// 		console.log(await fs.stat(file));
-// 	})
-// 	level++;
-// 	list.folders.forEach(async folder => {
-// 		if (folder)
-// 			recursiveLs(folder, fs, level);
-// 	})
-// }
-
-// async function recusriveFill(folder: TFolder, doc: YMap<ArrayBuffer>, fs: FileSystemAdapter) {
-// 	folder.children.forEach(async child => {
-// 		// console.log(child);
-// 		if ("children" in child)
-// 			await recusriveFill(child as TFolder, doc, fs);
-// 		else
-// 			console.log("content of " + child.path + ": " + await fs.readBinary(child.path))
-// 			// doc.set(child.path, await fs.readBinary(child.path));
-// 			// doc.set(child.path, 0 as ArrayBuffer);
-// 	})
-// }
-
-
 export default class MyPlugin extends Plugin {
 	basePath: string = "Dev/obsidian_dev";
 	ydoc: Y.Doc = new Y.Doc;
-	ymap: YMap<string> = this.ydoc.getMap("markdown");
 	settings: MyPluginSettings;
 	distant_provider: WebsocketProvider;
-	fs: FileSystemAdapter = new FileSystemAdapter(".");
+	obsidianProvider: ObsidianProvider = new ObsidianProvider(this.app, this.ydoc, this.basePath);
 
 	async onload() {
 		// const basePath = (this.app.vault.adapter as any).basePath;
 		// this.distant_provider = new WebsocketProvider('ws://localhost:8080', "vault_name", this.ydoc);
-		await this.initDoc();
-		console.log(this.ymap);
-		this.app.vault.on("modify", async (file: TFile) => this.setYFile(file), this);
-		this.ydoc.on('update', (update, origin) => {
-			console.log(origin + ": " + update);
-		})
 		await this.loadSettings();
 	}
 
 	onunload() {
+		this.obsidianProvider.destroy();
+		this.ydoc.destroy();
 	}
 
-	async initDoc() {
-		this.app.vault.getMarkdownFiles().forEach(async file => this.setYFile(file))
-	}
-
-	async setYFile(file: TFile) {
-		console.log(this.ydoc.getText("markdown"));
-		if (this.ymap && this.app)
-			this.ymap.set(file.path, await this.app.vault.read(file));
-	}
-
-	// async populateModifToYdoc(file: TFile) {
-	// 	if (!this)
-	// 		return ;
-	// 	let delta: YDelta[] = [];
-	// 	if (this.ymap && this.ymap.has(file.path))
-	// 		delta = getDeltaOperations(this.ymap.get(file.path) as string, await this.app.vault.read(file));
-	// 	if (delta.length > 0)
-	// 		this.ymap.set(file.path, )
-	// 	console.log(delta);
-	// }
-
-	// async recusriveFill(folder: TFolder) {
-	// 	folder.children.forEach(async child => {
-	// 		// console.log(child);
-	// 		if ("children" in child)
-	// 			await this.recusriveFill(child as TFolder);
-	// 		else
-	// 			// console.log("content of " + child.path + ": " + await this.fs.readBinary("./" + child.path))
-	// 			console.log(await this.fs.list(this.basePath));
-	// 			// console.log(await this.fs.readBinary(this.basePath + child.path))
-	// 			// doc.set(child.path, await fs.readBinary(child.path));
-	// 			// doc.set(child.path, 0 as ArrayBuffer);
-	// 	})
-	// }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
